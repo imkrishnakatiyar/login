c7ccf75ca83ebe10caca1617995cf6bf5b2bddb2
imkrishnakatiyar <90976299+imkrishnakatiyar@users.noreply.github.com>
2025-08-25 20:25:25 +0530

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,7 @@
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+  // Optionally, add client-side validation here
+});
+
+document.getElementById("registerForm").addEventListener("submit", (e) => {
+  // Optionally, add client-side validation here
+});
